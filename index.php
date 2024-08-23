1c4892d07185d7e9443d5e8278e5ceec71268074
everton-ods <soeblues4.eo@gmail.com>
2024-08-23 11:34:09 -0300

    update

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 session_start();
 require_once("vendor/autoload.php");
 
@@ -12,46 +12,90 @@ $app = new Slim();
 
 $app->config('debug', true);
 
-$app->get('/', function() {
+$app->get('/', function () {
     $page = new Page();
-	$page->setTpl('index');
-	
+    $page->setTpl('index');
 });
 
-$app->get('/admin', function() {
+$app->get('/admin', function () {
     User::verifyLogin();
     $page = new PageAdmin();
-	$page->setTpl('index');
-
-	
+    $page->setTpl('index');
 });
 
-$app->get('/admin/login', function() {
+$app->get('/admin/login', function () {
     $page = new PageAdmin([
         "header" => false,
-        "footer"=> false,
+        "footer" => false,
     ]);
-	$page->setTpl("login");
-    
-	
+    $page->setTpl("login");
 });
-$app->post('/admin/login', function() {
-    
+$app->post('/admin/login', function () {
+
     User::login($_POST["login"], $_POST["password"]);
 
     header("Location: /admin");
     exit;
-    
-	
 });
-$app->get('/admin/logout', function() {
+$app->get('/admin/logout', function () {
     User::logout();
-	header("Location: /admin/login");
+    header("Location: /admin/login");
     exit;
-    
-	
 });
+$app->get("/admin/users", function () {
+    User::verifyLogin();
+    $users = User::listAll();    
+    $page = new PageAdmin();
+    $page->setTpl("users", array(
+        "users"=> $users
+    ));
+});
+$app->get("/admin/users/:iduser/delete", function($iduser){
+    User::verifyLogin();
+    $user = new User() ;
+    $user->get((int)$iduser);
+    $user->delete();    
+    header("Location: /admin/users");
+    exit;
+});
+$app->get("/admin/users/:iduser", function($iduser) {
+    User::verifyLogin();
+    $user = new User();
+    $user->get((int)$iduser);
+    $page = new PageAdmin();
+    $page ->setTpl("users-update", array(
+        "user"=>$user->getValues(),
+    ));
+});
+$app->get("/admin/create", function() {
+    User::verifyLogin();
+    $page = new PageAdmin();
+    $page->setTpl("users-create");
+});
+$app->post("/admin/users/create", function(){
+    User::verifyLogin();
+    $user = new User();
+    $_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0 ;
+    $_POST['despassword'] = password_hash($_POST["despassword"], PASSWORD_DEFAULT, [
+        "cost"=>12
+    ]);
+    $user->setData($_POST);
+    $user->save();
+    header("Location: /admin/users");
+    exit;
+});
+$app->post("/admin/users/:iduser", function($iduser){
+    User::verifyLogin();
+    $user = new User();
+    $user->get((int)$iduser);
+    $user->setData($_POST);
+    $user->update();
+    header("Location: /admin/users");
+    exit;
+});
+
+
+
+
 
 $app->run();
-
- 
